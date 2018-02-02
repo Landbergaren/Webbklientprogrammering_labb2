38396327b7f36a6256bf27bc21ca0ad9511d0a2a
Tomas Landberg <landbergaren@gmail.com>
2018-02-02 20:06:26 +0100

    fixed 2 functions

--- scripts.js
+++ scripts.js
@@ -4,27 +4,8 @@ window.onload = function () {
     //functions
     function callBack(httpReq) {
         console.log(httpReq);        
-        let content = JSON.parse(httpReq);
-        console.log("this is after: ", content);
-        let myDiv = document.getElementById("comment-box")
-        for (var i = 0; i < content.length; i++) {
-            var commentBox = document.createElement("div");
-            var nameBox = document.createElement("div");
-            myDiv.appendChild(commentBox);               
-                var likes = document.createTextNode(" Likes: " + content[i]["likes"]);
-                var commenter = document.createTextNode(content[i]["commenter"]);
-                var message = document.createTextNode(content[i]["message"]);
-                nameBox.appendChild(commenter);
-                nameBox.appendChild(likes);
-                commentBox.appendChild(message);
-                myDiv.appendChild(nameBox);
-                myDiv.appendChild(commentBox);
-
-                console.log(commenter);
-           
-            myDiv.insertAdjacentHTML('beforeend', '<br />');
-        };
-
+        let content = JSON.parse(httpReq);       
+        console.log("this is after parse: ", content);
 
     };
 
@@ -43,6 +24,35 @@ window.onload = function () {
 
         http.send(null);
     };
+
+    function recursion (n) {
+        if (n <= 1){
+            return 1;
+        };
+        return n * factorial(n-1);
+    }
+
+    function renderElements () {
+        let myDiv = document.getElementById("comment-box")
+        for (var i = 0; i < content.length; i++) {
+            
+            var commentBox = document.createElement("div");
+            var nameBox = document.createElement("div");
+            myDiv.appendChild(commentBox);               
+                var likes = document.createTextNode(" Likes: " + content[i]["likes"]);
+                var commenter = document.createTextNode(content[i]["commenter"]);
+                var message = document.createTextNode(content[i]["message"]);
+                nameBox.appendChild(commenter);
+                nameBox.appendChild(likes);
+                commentBox.appendChild(message);
+                myDiv.appendChild(nameBox);
+                myDiv.appendChild(commentBox);
+
+                console.log(commenter);
+           
+            myDiv.insertAdjacentHTML('beforeend', '<br />');
+        };
+    }
 
     //Exec below
     httpGet("http://localhost:8080/shallow", callBack);
