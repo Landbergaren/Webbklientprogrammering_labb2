3ce8130d06fb325b7e390824953060fce034aa9a
Tomas Landberg <landbergaren@gmail.com>
2018-02-04 15:10:03 +0100

    rearranged body, like implemented, functions moved out of onload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,114 @@
+
+function hide(id) {
+    let currentAttr = document.getElementById(id).getAttribute("style");
+    let commentElement = document.getElementById(id.toString())
+
+    if (currentAttr !== "display: none;") {
+        commentElement.style.display = "none";
+        this.value = "det funka";
+        //document.querySelector(`"#${id} button"`).setAttribute("")
+    }
+    else {
+        commentElement.style.display = "inline-block";
+    }
+}
+
+function likeComment(id) {
+    httpPost("http://localhost:8080/like/" + id, null, callBack);
+}
+
+function httpPost(theUrl, params, callback) {
+    const http = new XMLHttpRequest();
+    http.open('POST', theUrl, true);
+    document.body.innerHTML = '<div id="comment-box"></div>';
+    http.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200)            
+            callback(JSON.parse(http.responseText));
+    };
+
+    http.send(params);
+}
+
+function callBack(parsedGet) {
+    console.log("this is parsedGet")
+    console.log(parsedGet);
+    recursion(parsedGet, "");
+};
+
+function recursion(n, level) {
+    if (n === undefined) {
+        console.log("and now we are undefined")
+        return;
+    }
+    if (Array.isArray(n)) {
+        for (var comment of n) {
+            let commentBox = renderSingleElement("div", level)
+            recursion(comment, level);
+        }
+    }
+    else {
+        renderAllComments(n, level);
+        recursion(n.answers, level + "----------");
+    }
+}
+
+function renderAllComments(objectToPrint, level) {
+
+    let myDiv = document.getElementById("comment-box")
+
+    //Create
+    let outerBox = document.createElement("div");
+    let commentBox = document.createElement("div");
+    let nameBox = document.createElement("div");
+    let contentBox = document.createElement("p");
+    nameBox.classList.add("comment-box");
+    myDiv.appendChild(commentBox);
+    let likes = document.createTextNode(objectToPrint["likes"]);
+    let likesElement = document.createElement("div");
+    let commenter = document.createTextNode(objectToPrint["commenter"]);
+    let message = document.createTextNode(objectToPrint["message"]);
+    let likeButton = document.createElement("button");
+    let likeButtonTxt = document.createTextNode("Like");
+    let hideButton = document.createElement("button");
+    let hideButtonTxt = document.createTextNode("Hide/show");
+
+    //Append
+    likesElement.appendChild(likes)
+    likeButton.appendChild(likeButtonTxt);
+    outerBox.appendChild(hideButton);
+    hideButton.setAttribute("onclick", `hide("${objectToPrint.id}")`)
+    hideButton.appendChild(hideButtonTxt);
+    likeButton.setAttribute("onclick", `likeComment("${objectToPrint.id}")`)
+    nameBox.appendChild(commenter);
+    nameBox.appendChild(likesElement);
+
+    likesElement.appendChild(likeButton);
+    commentBox.appendChild(nameBox);
+    contentBox.appendChild(message);
+    commentBox.appendChild(contentBox);
+    commentBox.setAttribute("id", `${objectToPrint.id}`);
+    outerBox.appendChild(commentBox);
+    myDiv.appendChild(outerBox);
+
+    myDiv.insertAdjacentHTML('beforeend', '<br />');
+
+}
+
+function renderSingleElement(elementType, message) {
+    let innerDiv = document.getElementById("comment-box")
+    let newElement = document.createElement(elementType)
+    let newTextNode = document.createTextNode(`${message}`);
+    innerDiv.appendChild(newTextNode);
+    return innerDiv;
+}
+
+
 //onload
 window.onload = function () {
     //functions
-    function callBack(parsedGet) {
-        console.log("this is parsedGet")
-        console.log(parsedGet);
-        recursion(parsedGet, "");
 
-    };
 
     function httpGet(theUrl, callback) {
         const http = new XMLHttpRequest();
@@ -21,30 +123,10 @@ window.onload = function () {
         http.send(null);
     };
 
-    function httpPost(theUrl, params, callback) {
-        const http = new XMLHttpRequest();
-        http.open('POST', theUrl, true);
 
-        http.setRequestHeader()
-    }
 
     //min tidigare recursion
-    function recursion(n, level) {
-        if (n === undefined) {
-            console.log("and now we are undefined")
-            return;
-        }
-        if (Array.isArray(n)) {
-            for (var comment of n) {
-                let commentBox = renderSingleElement("div", level)
-                recursion(comment, level);
-            }
-        }
-        else {
-            renderAllComments(n, level);
-            recursion(n.answers, level + "----------");
-        }
-    }
+
 
     // function recursion(level, node) {
     //     if (Array.isArray(node)) {
@@ -72,70 +154,11 @@ window.onload = function () {
     //     }
     //   }
 
-    function renderAllComments(objectToPrint, level) {
-        let myDiv = document.getElementById("comment-box")
-
-        //Create
-        let outerBox = document.createElement("div");
-        let commentBox = document.createElement("div");
-        let nameBox = document.createElement("div");
-        let contentBox = document.createElement("p");
-        nameBox.classList.add("comment-box");
-        myDiv.appendChild(commentBox);
-        let likes = document.createTextNode(objectToPrint["likes"]);
-        let likesElement = document.createElement("div");
-        let commenter = document.createTextNode(objectToPrint["commenter"]);
-        let message = document.createTextNode(objectToPrint["message"]);
-        let likeButton = document.createElement("button");
-        let likeButtonTxt = document.createTextNode("Like");
-        let hideButton = document.createElement("button");
-        let hideButtonTxt = document.createTextNode("Hide/show");
-
-        //Append
-        likesElement.appendChild(likes)
-        likeButton.appendChild(likeButtonTxt);
-        outerBox.appendChild(hideButton);
-        hideButton.setAttribute("onclick", `hide("${objectToPrint.id}")`)        
-        hideButton.appendChild(hideButtonTxt);
-        nameBox.appendChild(commenter);
-        nameBox.appendChild(likesElement);
-
-        likesElement.appendChild(likeButton);
-        commentBox.appendChild(nameBox);
-        contentBox.appendChild(message);
-        commentBox.appendChild(contentBox);
-        commentBox.setAttribute("id", `${objectToPrint.id}`);
-        outerBox.appendChild(commentBox);
-        myDiv.appendChild(outerBox);
-
-        myDiv.insertAdjacentHTML('beforeend', '<br />');
-
-    }
-
-    function renderSingleElement(elementType, message) {
-        let innerDiv = document.getElementById("comment-box")
-        let newElement = document.createElement(elementType)
-        let newTextNode = document.createTextNode(`${message}`);
-        innerDiv.appendChild(newTextNode);
-        return innerDiv;
-    }
+    
 
     //Exec below
     httpGet("http://localhost:8080/deep", callBack);
 
 };
 
-function hide(id) {
-    let currentAttr = document.getElementById(id).getAttribute("style");
-    let commentElement = document.getElementById(id.toString())
-    console.log(currentAttr);
-    if (currentAttr !== "display: none;") {
-        commentElement.style.display = "none";
-        this.value="det funka";
-        //document.querySelector(`"#${id} button"`).setAttribute("")
-    }
-    else {
-        commentElement.style.display = "inline-block";
-    }
-}
 
